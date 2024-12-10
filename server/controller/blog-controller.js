269f83eb1269be5d6e8b70963535deb7cc17701f
zohaibmannna99 <zohaibfencer@gmail.com>
2024-12-10 15:15:42 -0500

    Added functions for fetchListOfBlogs, deleteBlog, updateBlog, and addNewBlog

--- server/controller/blog-controller.js
+++ server/controller/blog-controller.js
@@ -22,3 +22,74 @@ const fetchListofBlogs = async(req,res)=>{
 
 };
 
+const addNewBlog = async(req,res) =>{
+    const {title, description} = req.body;
+    const currentDate = new Date();
+
+    const newlyCreatedBlog = new Blog({
+        title, description, date : currentDate
+    })
+
+    try {
+        await newlyCreatedBlog.save()
+    }
+    catch(e){
+        console.loge(e)
+    }
+
+    try{
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        await newlyCreatedBlog.save(session);
+        session.commitTransaction();
+
+    }
+    catch(e){
+        return res.senc(500).json({message : e});
+    }
+
+    return res.status(200).json({newlyCreatedBlog});
+};
+
+const deleteBLog = async(req,res) =>{
+    const id = req.params.id;
+
+    try{
+        const findCurrentBlog = await Blog.findByIdAndDelete(id);
+        if(!findCurrentBlog){
+            return res.status(404).json({message: 'Blog not found'})
+        }
+
+        return res.status(200).json({message: "Successfully Deleted"})
+    } catch(e){
+        console.log(e);
+        return res.status(500).json({message: 'Unable to delete! Try Again'})
+    }
+};
+
+const updateBlog = async(req,res)=>{
+    const id = req.params.id;
+
+    const {title, description} = req.body
+    let currentBlogToUpdate
+
+    try{
+        currentBlogToUpdate = await Blog.findByIdAndUpdate(id, {
+            title, description
+        })
+    }catch(e){
+        console.log(e)
+
+        return res.send(500).json({message: 'Something went wrong. Please try again'}); 
+    }
+
+    if(!currentBlogToUpdate){
+        return res.status(500).json({message: "Unable to update"});
+    } 
+
+    return res.send(200).json(currentBlogToUpdate);
+};
+
+module.exports = {fetchListofBlogs, deleteBLog, updateBlog, addNewBlog};
+
+
